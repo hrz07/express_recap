df96ebaa085e3e48b6093b2ac2affb9f500f5dfc
Rashedul Islam Hridoy <hridoy.developer@yahoo.com>
2026-09-18 23:45:35 +0600

    Add EJS templating support and create index.ejs view

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const helmet = require('helmet');
+const path = require('path');
 
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'temp_files/ejs'));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.get('/ejs', (req, res) => {
+  res.render('index');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

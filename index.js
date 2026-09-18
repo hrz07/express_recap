3513113071356fd0bc25101218620a9ed28fa290
Rashedul Islam Hridoy <hridoy.developer@yahoo.com>
2026-09-18 23:22:14 +0600

    Add helmet middleware for enhanced security and update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const helmet = require('helmet');
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
